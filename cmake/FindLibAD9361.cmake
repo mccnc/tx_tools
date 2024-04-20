d1aaae64094702a2782bd8155f9b2412a12cbe51
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2024-04-20 14:32:10 +0200

    Add build check Github Action

--- cmake/FindLibAD9361.cmake
+++ cmake/FindLibAD9361.cmake
@@ -1,10 +1,11 @@
 # - Try to find libad9361-iio
 # Once done this will define
 #
-#  LibAD9361_FOUND - system has libiio
-#  LibAD9361_INCLUDE_DIRS - the libiio include directory
-#  LibAD9361_LIBRARIES - Link these to use libiio
-#  LibAD9361_DEFINITIONS - Compiler switches required for using libiio
+#  LibAD9361_FOUND - system has libad9361
+#  LibAD9361_INCLUDE_DIRS - the libad9361 include directory
+#  LibAD9361_LIBRARIES - Link these to use libad9361
+#  LibAD9361_DEFINITIONS - Compiler switches required for using libad9361
+#  LibAD9361_VERSION - the libad9361 version
 #
 # Redistribution and use is allowed according to the terms of the New BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -14,17 +15,38 @@ find_package(PkgConfig)
 pkg_check_modules(PC_LibAD9361 QUIET libad9361)
 set(LibAD9361_DEFINITIONS ${PC_LibAD9361_CFLAGS_OTHER})
 
+# $ENV{HOMEBREW_PREFIX} can be /opt/homebrew (arm64) or defaults to /usr/local
+set(HOMEBREW_PREFIX $ENV{HOMEBREW_PREFIX})
+if(NOT HOMEBREW_PREFIX)
+    set(HOMEBREW_PREFIX "/usr/local")
+endif()
+
+# Note: a 0.2 version might be named 0.3 and a 0.1 might be missing the version
 find_path(LibAD9361_INCLUDE_DIR ad9361.h
           HINTS ${PC_LibAD9361_INCLUDEDIR} ${PC_LibAD9361_INCLUDE_DIRS}
+          PATHS
+          /opt/local/Library/Frameworks
+          ${HOMEBREW_PREFIX}/Cellar/libad9361-iio/${PC_LibIIO_VERSION}/Frameworks
+          ${HOMEBREW_PREFIX}/Cellar/libad9361-iio/0.3/Frameworks
+          ${HOMEBREW_PREFIX}/Cellar/libad9361/${PC_LibIIO_VERSION}
+          ${HOMEBREW_PREFIX}/Cellar/libad9361/0.1
           PATH_SUFFIXES libad9361-iio)
 
 find_library(LibAD9361_LIBRARY NAMES ad9361 libad9361
-             HINTS ${PC_LibAD9361_LIBDIR} ${PC_LibAD9361_LIBRARY_DIRS})
+             HINTS ${PC_LibAD9361_LIBDIR} ${PC_LibAD9361_LIBRARY_DIRS}
+             PATHS
+             /opt/local/Library/Frameworks
+             ${HOMEBREW_PREFIX}/Cellar/libad9361-iio/${PC_LibIIO_VERSION}/Frameworks
+             ${HOMEBREW_PREFIX}/Cellar/libad9361-iio/0.3/Frameworks
+             ${HOMEBREW_PREFIX}/Cellar/libad9361/${PC_LibIIO_VERSION}
+             ${HOMEBREW_PREFIX}/Cellar/libad9361/0.1)
 
 set(LibAD9361_VERSION ${PC_LibAD9361_VERSION})
 
 include(FindPackageHandleStandardArgs)
+# Note that `FOUND_VAR LibIIO_FOUND` is needed for cmake 3.2 and older.
 find_package_handle_standard_args(LibAD9361
+                                  FOUND_VAR LibAD9361_FOUND
                                   REQUIRED_VARS LibAD9361_LIBRARY LibAD9361_INCLUDE_DIR
                                   VERSION_VAR LibAD9361_VERSION)
 
--- cmake/FindLibIIO.cmake
+++ cmake/FindLibIIO.cmake
@@ -5,6 +5,7 @@
 #  LibIIO_INCLUDE_DIRS - the libiio include directory
 #  LibIIO_LIBRARIES - Link these to use libiio
 #  LibIIO_DEFINITIONS - Compiler switches required for using libiio
+#  LibIIO_VERSION - the libiio version
 #
 # Redistribution and use is allowed according to the terms of the New BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -14,17 +15,33 @@ find_package(PkgConfig)
 pkg_check_modules(PC_LibIIO QUIET libiio)
 set(LibIIO_DEFINITIONS ${PC_LibIIO_CFLAGS_OTHER})
 
+# $ENV{HOMEBREW_PREFIX} can be /opt/homebrew (arm64) or defaults to /usr/local
+set(HOMEBREW_PREFIX $ENV{HOMEBREW_PREFIX})
+if(NOT HOMEBREW_PREFIX)
+    set(HOMEBREW_PREFIX "/usr/local")
+endif()
+
 find_path(LibIIO_INCLUDE_DIR iio.h
           HINTS ${PC_LibIIO_INCLUDEDIR} ${PC_LibIIO_INCLUDE_DIRS}
+          PATHS
+          /opt/local/Library/Frameworks
+          ${HOMEBREW_PREFIX}/Cellar/libiio/${PC_LibIIO_VERSION}/Frameworks
+          ${HOMEBREW_PREFIX}/Cellar/libiio/${PC_LibIIO_VERSION}
           PATH_SUFFIXES libiio)
 
 find_library(LibIIO_LIBRARY NAMES iio libiio
-             HINTS ${PC_LibIIO_LIBDIR} ${PC_LibIIO_LIBRARY_DIRS})
+             HINTS ${PC_LibIIO_LIBDIR} ${PC_LibIIO_LIBRARY_DIRS}
+             PATHS
+             /opt/local/Library/Frameworks
+             ${HOMEBREW_PREFIX}/Cellar/libiio/${PC_LibIIO_VERSION}/Frameworks
+             ${HOMEBREW_PREFIX}/Cellar/libiio/${PC_LibIIO_VERSION})
 
 set(LibIIO_VERSION ${PC_LibIIO_VERSION})
 
 include(FindPackageHandleStandardArgs)
+# Note that `FOUND_VAR LibIIO_FOUND` is needed for cmake 3.2 and older.
 find_package_handle_standard_args(LibIIO
+                                  FOUND_VAR LibIIO_FOUND
                                   REQUIRED_VARS LibIIO_LIBRARY LibIIO_INCLUDE_DIR
                                   VERSION_VAR LibIIO_VERSION)
 
